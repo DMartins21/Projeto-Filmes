b7c6c15cf4040eedfd519c2a08c265aaac338dfe
Davi <davi_martins2009@outlook.com>
2023-02-09 16:15:12 -0300

    Listando Filmes

--- src/Pages/Home/Index.js
+++ src/Pages/Home/Index.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react"
 import api from '../../Services/api'
+import { Link } from "react-router-dom"
+import './Home.css'
 //https://api.themoviedb.org/3/movie/550?api_key=1b23c66281853320747b15d6d16228fa
 
 function Home(){
@@ -15,13 +17,24 @@ function Home(){
                     page:1,
                 }
             })
-            console.log(response.data)
+           setFilmes(response.data.results.slice(0,10))
         }
         loadFilmes()
     },[])
     return(
-        <div>
-            <h1>Bem Vindo ao Home</h1>
+        <div className="container">
+            <div className="lista-filmes">
+                {filmes.map((filme) =>{
+                    return(
+                        <article key={filme.id}>
+                            <strong>{filme.title}</strong>
+                            <img src=
+                            {`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+                            <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                        </article>
+                    )
+                })}
+            </div>
         </div>
     )
 }
